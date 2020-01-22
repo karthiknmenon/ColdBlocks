440b7ee4826c5dacbd593dc1137db22c6b9f31c4
mojojojo20 <menonk484@gmail.com>
2020-01-22 11:24:28 +0530

    add whatsapp and sms

--- coldblocks-nodejs/index.js
+++ coldblocks-nodejs/index.js
@@ -4,8 +4,24 @@ const accountSid = 'ACe72fa712ccadfebda35cfee9aeb3c9d4';
 const authToken = '13b758715b01c14a7be78f911495c0d2';
 const client = require('twilio')(accountSid, authToken);
 
-client.messages.create({
-     from: 'whatsapp:+14155238886',
-     body: 'Hello World',
-     to: 'whatsapp:+919586976787'
-   }).then(message => console.log(message.sid));
+// function to send messages via whatsapp
+function sendWhatsapp() {
+    client.messages.create({
+        from: 'whatsapp:+14155238886',
+        body: 'Your package is at Delhi, temperature 25*C',
+        to: 'whatsapp:+919586976787'
+    }).then(message => console.log(message.sid));
+}
+
+// functon to send SMS
+function sendSMS() {
+    client.messages
+        .create({
+            body: 'Your package is at Delhi, temperature 25*C',
+            from: '+14155238886',
+            to: '+919586976787'
+        })
+        .then(message => console.log(message.sid));
+
+}
+sendSMS();
