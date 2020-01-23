9b659256d35e51469ec786b90b4600ac8b322601
mojojojo20 <menonk484@gmail.com>
2020-01-23 12:45:41 +0530

    print values

--- coldblocks-nodejs/index.js
+++ coldblocks-nodejs/index.js
@@ -18,6 +18,8 @@ function sendWhatsapp() {
     }).then(message => console.log(message.sid));
 }
 
+// sendWhatsapp();
+
 // functon to send SMS
 // function sendSMS() {
 //     client.messages
@@ -33,8 +35,42 @@ function sendWhatsapp() {
 
 // API to print all consumers
 
-app.get("/", (request,resposne)=>{
-    resposne.send("hello world");
-})
+app.get('/api', function(req, res) {
+    
+    
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                
+            axios.get('http://localhost:3000/api/Consumer').then(function (response){
+                console.log(response.data);
+                jsonResponse = response.data;
+                // res.send(jsonResponse);
+                // var obj = jsonResponse['consumerID'];
+                console.log(jsonResponse[0]['consumerID']);
+                // res.send(jsonResponse[0]['consumerID']);
+            }).then(function (response){
+                // res.send(jsonResponse[0]['consumerID']);
+                showID();
+            }).catch(function (error) {
+            console.log(error);
+          });
+
+          function showID(){
+            //   for(var i=0;i<2;i++)
+            //   {res.send(jsonResponse[2]['consumerID']);}
+            // console.log(jsonResponse.length);
+            var arr = new Array();
+            for(var i=0;i<jsonResponse.length;i++){
+                arr.push(jsonResponse[i]['consumerID'].replace(/\"/g, ""));
+            }
+            res.send(arr);
+          }
+
+            // var obj = JSON.parse(jsonResponse);
+            // console.log(obj.consumerID);
+            
+});
+
 
 app.listen(4000);
