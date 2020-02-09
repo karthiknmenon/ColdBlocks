e5ffdcda99339d30bb09bfe6cb3b4707ccb3cc90
mojojojo20 <menonk484@gmail.com>
2020-02-09 20:59:21 +0530

    user profile

--- coldblocks-react/src/views/UserProfile.jsx
+++ coldblocks-react/src/views/UserProfile.jsx
@@ -153,30 +153,26 @@ class UserProfile extends Component {
               <UserCard
                 bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
                 avatar={avatar}
-                name="Mike Andrew"
-                userName="michael24"
+                name="Karthik Menon"
+                userName="S101"
                 description={
                   <span>
-                    "Lamborghini Mercy
-                    <br />
-                    Your chick she so thirsty
-                    <br />
-                    I'm in that two seat Lambo"
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   </span>
                 }
-                socials={
-                  <div>
-                    <Button simple>
-                      <i className="fa fa-facebook-square" />
-                    </Button>
-                    <Button simple>
-                      <i className="fa fa-twitter" />
-                    </Button>
-                    <Button simple>
-                      <i className="fa fa-google-plus-square" />
-                    </Button>
-                  </div>
-                }
+                // socials={
+                //   <div>
+                //     <Button simple>
+                //       <i className="fa fa-facebook-square" />
+                //     </Button>
+                //     <Button simple>
+                //       <i className="fa fa-twitter" />
+                //     </Button>
+                //     <Button simple>
+                //       <i className="fa fa-google-plus-square" />
+                //     </Button>
+                //   </div>
+                // }
               />
             </Col>
           </Row>
