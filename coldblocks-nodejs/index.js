61f0d0bd883735d22b8d5080713a7a355fe3adf6
mojojojo20 <menonk484@gmail.com>
2020-02-25 13:45:07 +0530

    :fire: working qr code feature

--- coldblocks-nodejs/index.js
+++ coldblocks-nodejs/index.js
@@ -10,22 +10,24 @@ var crypto = require('crypto');
 // var crypto = require('crypto');
 var aes256 = require('aes256');
 var QRCode = require('qrcode');
+QRCode.toString('http://b7a7e53d.ngrok.io/HolderChange', {
+    type: 'terminal'
+}, function (err, url) {
+    console.log(url)
+});
 // QRCode.toDataURL('https://www.google.com!', function (err, url) {
 //     console.log(url)
 //   });
-   
-QRCode.toString('https://www.google.com',{type:'terminal'}, function (err, url) {
-    console.log(url)
-  });
 
-var key = 'my passphrase';
-var plaintext = 'my plaintext message';
- 
-var encrypted = aes256.encrypt(key, plaintext);
-var decrypted = aes256.decrypt(key, encrypted);
 
-console.log(encrypted);
-console.log(decrypted);
+// var key = 'my passphrase';
+// var plaintext = 'my plaintext message';
+
+// var encrypted = aes256.encrypt(key, plaintext);
+// var decrypted = aes256.decrypt(key, encrypted);
+
+// console.log(encrypted);
+// console.log(decrypted);
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -51,7 +53,7 @@ app.get("/", (req, res) => {
     res.send("Server Running");
 })
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Server running");
 })
 
@@ -783,5 +785,36 @@ app.get('/tempDrop', function (req, res) {
         console.log(error);
     });
 })
+
+app.post('/HolderChange', function (req, res) {
+    // console.log(JSON.stringify(req.body));
+    // var oHolder = req.body.oldHolder;
+    var oHolder = "hyder";
+    console.log("oldHolder: " + oHolder);
+    // var packageID = req.body.packageID;
+    var packageID = "H156";
+    console.log("Package Id: " + packageID);
+    // var nHolder = req.body.newHolder;
+    var nHolder = "dsdsds";
+    console.log("Location: " + nHolder);
+    Request.post({
+        "headers": {
+            "content-type": "application/json"
+        },
+        "url": restUrl + "api/HolderChange",
+        "body": JSON.stringify({
+            "asset": "resource:org.coldblocks.mynetwork.TransitPackage#" + packageID,
+            "oldHolder": String(oHolder),
+            "newHolder": String(nHolder)
+        })
+    }, (error, response, body) => {
+        if (error) {
+            return console.dir(error);
+        }
+    });
+});
+
+
+
 
 app.listen(4000);
