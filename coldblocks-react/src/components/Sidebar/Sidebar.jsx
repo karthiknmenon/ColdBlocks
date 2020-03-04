1199fe2fb56702f3aec50c03c76682586cc89b1b
mojojojo20 <menonk484@gmail.com>
2020-03-04 16:11:19 +0530

    :construction: change links

--- coldblocks-react/src/components/Sidebar/Sidebar.jsx
+++ coldblocks-react/src/components/Sidebar/Sidebar.jsx
@@ -41,7 +41,7 @@ class Sidebar extends Component {
           )}
         <div className="logo">
           <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
+            href="https://wwww.coldblocks.tech"
             className="simple-text logo-mini"
           >
             <div className="logo-img">
@@ -49,7 +49,7 @@ class Sidebar extends Component {
             </div>
           </a>
           <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
+            href="https://wwww.coldblocks.tech"
             className="simple-text logo-normal"
           >
             ColdDash
