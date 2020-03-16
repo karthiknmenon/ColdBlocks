65fd24ef8cd12a8cc639f0f541eaa09cad7e5637
mojojojo20 <menonk484@gmail.com>
2020-03-16 15:51:52 +0530

    :bug: fix AR issue

--- coldblocks-react/src/views/camera.jsx
+++ coldblocks-react/src/views/camera.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Webcam from "react-webcam";
 import {
   Grid,
   Row,
@@ -7,14 +6,9 @@ import {
   Button
 } from "react-bootstrap";
 import { Card } from "components/Card/Card.jsx";
-// import Button from "components/CustomButton/CustomButton"
  
-class WebcamCapture extends React.Component {
+class ColdAr extends React.Component {
   render() {
-    const videoConstraints = {
-      facingMode: "environment"
-    };
-
     return (
       <div className="content">
           <Grid fluid>
@@ -23,16 +17,15 @@ class WebcamCapture extends React.Component {
               <Card
                   category="ColdAR is an Augmented Reality enabled QR Scanner that allows a user to scan a product QR code to verify its status. A status -ok- means that the package is not tampered, else it's tampered."
                   content={
-                    <Button  pullRight bsStyle="primary">Launch ColdAR</Button>
+                    <Button  pullRight bsStyle="primary" href="https://coldblocksar.netlify.com" target="_blank">Launch ColdAR</Button>
                   }
                 />
               </Row>
             </Col>
           </Grid>
-        </div>
-          
+        </div>          
     )
   }
 }
 
-export default WebcamCapture;
+export default ColdAr;
