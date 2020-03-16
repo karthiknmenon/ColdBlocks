3ca1efe5f89c87a11b309fe9aba0fb75ed1d1de5
mojojojo20 <menonk484@gmail.com>
2020-03-16 15:08:01 +0530

    :boom: initial analysis commit

--- coldblocks-react/src/views/DataChart.jsx
+++ coldblocks-react/src/views/DataChart.jsx
@@ -1,17 +1,41 @@
 
 import React, { Component } from "react";
-import { Table, Grid, Row, Col } from "react-bootstrap";
+import {Grid, Row, Col } from "react-bootstrap";
 import Card from "components/Card/Card";
 import Button from "components/CustomButton/CustomButton";
+import ChartistGraph from 'react-chartist';
 
 class Chart extends Component {
   render() {
+    var data = {
+      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+      series: [
+        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+      ]
+    };
+
+    var options = {
+      high: 10,
+      low: -10,
+      axisX: {
+        labelInterpolationFnc: function(value, index) {
+          return index % 2 === 0 ? value : null;
+        }
+      }
+    };
+
+    var type = 'Bar'
     return (
+  
       <div className="content">
           <Grid>
             <Row>
               <Col md={12}>
-                  
+              <div className="row">
+              <div>
+        <ChartistGraph data={data} options={options} type={type} />
+      </div>
+              </div>
               </Col>
             </Row>
           </Grid>
