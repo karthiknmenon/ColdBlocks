b91bc2fac3df40b8aec7a36029d0f002af5fb19e
mojojojo20 <menonk484@gmail.com>
2020-03-16 13:45:28 +0530

    :poop: merge form and display for consumers

--- coldblocks-react/src/views/consumerlist.jsx
+++ coldblocks-react/src/views/consumerlist.jsx
@@ -1,17 +1,63 @@
 
 import React, { Component } from "react";
 import { Grid, Row, Col, Table } from "react-bootstrap";
-
-import Card from "components/Card/Card.jsx";
-import { thArray, tdArray } from "variables/Variables.jsx";
 import SendConsumerData from "./SendConsumerData";
+import { Card } from "components/Card/Card.jsx";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
+import axios from 'axios';
 
 class ConsumerList extends Component {
   constructor() {
     super()
     this.state = {
-      character:{}
+      character:{},
+      cName: '',
+      cID: '',
+      postD: 'null'
     }
+  }
+  nameChange = event => {
+    console.log("Ivnoked nameChange Event handleChange: "+event.target.value);
+    this.setState({ cName: event.target.value });
+
+  }
+  idChange = event => {
+    console.log("Invoked idChange Event handleChange: "+event.target.value);
+    this.setState({
+                    cID: event.target.value });
+
+  }
+ 
+  handleSubmit = event => {
+    event.preventDefault();
+    
+    console.log("state "+this.state.cID);
+    console.log("state "+this.state.cName);
+    const user = {
+      cID: String(this.state.cID),
+      cName: String(this.state.cName)
+    };
+    console.log("user "+user.cID);
+    console.log("user "+user.cName);
+    
+    axios.post(`http://localhost:4000/api/CreateConsumer`, 
+    { headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',}},
+    { data: user})
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+     
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    setTimeout(() => { this.setState({postD:'changed'}); }, 2000);
+    console.log(this.state.postD);
+    
   }
   componentDidMount() {
     // console.log("hi");
@@ -24,16 +70,71 @@ class ConsumerList extends Component {
     })
     .catch(console.log)
   }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.postD !== this.state.postD) {
+      console.log('postD state has changed.');
+      fetch('http://localhost:4000/api/ListConsumers')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ character: data })
+        console.log(this.state.character)
+      })
+      .catch(console.log)
+  }
+}
+
   render() {
     const {character} = this.state;
     return (
       <div className="content">
         <Grid fluid>
+          <Grid fluid>
           <Row>
             <Col md={12}>
-              <SendConsumerData></SendConsumerData>
+              <Card
+                title="Add Consumer"
+                content={
+                  <form onSubmit={this.handleSubmit} >
+                    <FormInputs 
+                      ncols={["col-md-5", "col-md-3", "col-md-4"]}
+                      properties={[
+                        {
+                          label: "Company (disabled)",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Company",
+                          defaultValue: "ColdBlocks",
+                          disabled: true
+                  
+                        },
+                        {
+                          label: "Consumer ID",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Consumer ID",
+                          onChange:this.idChange,
+                          name: "cID"
+                        },
+                        {
+                          label: "Name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Consumer Name",
+                          onChange:this.nameChange,
+                          name: "cName"
+                        }
+                      ]}
+                    />       
+                    <Button bsStyle="success" pullRight fill type="submit">
+                      Submit
+                    </Button>
+                    <div className="clearfix" />
+                  </form>
+                }
+              />
             </Col>
           </Row>
+        </Grid>
           <Row>
             <Col md={12}>
               <Card
