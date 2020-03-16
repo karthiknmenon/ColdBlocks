939443bfa7ab95eed5979971acafa4649c6eb899
mojojojo20 <menonk484@gmail.com>
2020-03-16 15:10:45 +0530

    :chart_with_upwards_trend: added line chart with dummy data

--- coldblocks-react/src/views/DataChart.jsx
+++ coldblocks-react/src/views/DataChart.jsx
@@ -7,24 +7,53 @@ import ChartistGraph from 'react-chartist';
 
 class Chart extends Component {
   render() {
+    // Create a line chart with responsive options
+
     var data = {
-      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+      labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
       series: [
-        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+          [287, 385, 490, 492, 554, 586, 698, 695],
+          [67, 152, 143, 240, 287, 335, 435, 437],
+          [23, 113, 67, 108, 190, 239, 307, 308]
       ]
-    };
-
-    var options = {
-      high: 10,
-      low: -10,
+  };
+    var optionsSales = {
+      low: 0,
+      high: 800,
+      showArea: false,
+      height: "245px",
       axisX: {
-        labelInterpolationFnc: function(value, index) {
-          return index % 2 === 0 ? value : null;
-        }
+          showGrid: false,
+      },
+      lineSmooth: true,
+      showLine: true,
+      showPoint: true,
+      fullWidth: true,
+      chartPadding: {
+          right: 50
       }
-    };
-
-    var type = 'Bar'
+  };
+    // In addition to the regular options we specify responsive option overrides that will override the default configutation based on the matching media queries.
+    var responsiveOptions = [
+      ['screen and (min-width: 641px) and (max-width: 1024px)', {
+        showPoint: false,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            // Will return Mon, Tue, Wed etc. on medium screens
+            return value.slice(0, 3);
+          }
+        }
+      }],
+      ['screen and (max-width: 640px)', {
+        showLine: false,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            // Will return M, T, W etc. on small screens
+            return value[0];
+          }
+        }
+      }]
+    ];
     return (
   
       <div className="content">
@@ -32,9 +61,12 @@ class Chart extends Component {
             <Row>
               <Col md={12}>
               <div className="row">
-              <div>
-        <ChartistGraph data={data} options={options} type={type} />
-      </div>
+              <ChartistGraph
+                data={data}
+                type="Line"
+                options={optionsSales}
+                responsiveOptions={responsiveOptions}
+                />
               </div>
               </Col>
             </Row>
