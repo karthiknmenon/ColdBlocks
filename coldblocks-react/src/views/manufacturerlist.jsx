2cbd1261dd8185430d3812da54e8ca9cae982b25
mojojojo20 <menonk484@gmail.com>
2020-03-16 14:21:21 +0530

    :zap: merge manufacturer form to list

--- coldblocks-react/src/views/manufacturerlist.jsx
+++ coldblocks-react/src/views/manufacturerlist.jsx
@@ -1,31 +1,120 @@
 
 import React, { Component } from "react";
 import { Grid, Row, Col, Table } from "react-bootstrap";
-
 import Card from "components/Card/Card.jsx";
-import { thArray, tdArray } from "variables/Variables.jsx";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
+import axios from 'axios';
 
 class ManufacturerList extends Component {
   constructor() {
     super()
     this.state = {
-      character:{}
+      apiData:{},
+      mId:{},
+      mName:{}
     }
   }
-  componentDidMount() {
+  nameChange = event => {
+    console.log("Ivnoked nameChange Event handleChange: "+event.target.value);
+    this.setState({ mName: event.target.value });
+
+  }
+  idChange = event => {
+    console.log("Invoked idChange Event handleChange: "+event.target.value);
+    this.setState({
+                    mId: event.target.value });
+
+  }
+ 
+  handleSubmit = event => {
+    event.preventDefault();
+    
+    console.log("state "+this.state.mId);
+    console.log("state "+this.state.mName);
+    const user = {
+      mId: String(this.state.mId),
+      mName: String(this.state.mName)
+    };
+    console.log("user "+user.mId);
+    console.log("user "+user.mName);
+    
+    axios.post(`http://localhost:4000/api/CreateManufacturer`, 
+    { headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',}},
+    { data: user})
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    
+  }
+  componentmIdMount() {
     // console.log("hi");
     fetch('http://localhost:4000/api/ListManufacturers')
     .then(res => res.json())
     .then((data) => {
-      this.setState({ character: data })
+      this.setState({ apiData: data })
       // console.log(data);
     })
     .catch(console.log)
   }
   render() {
-    const {character} = this.state;
+    const {apiData} = this.state;
     return (
-      <div className="content">
+        <div className="content">
+          <Grid fluid>
+            <Row>
+            <Col md={12}>
+              <Card
+                title="Add Distributor"
+                content={
+                  <form onSubmit={this.handleSubmit} >
+                    <FormInputs 
+                      ncols={["col-md-5", "col-md-3", "col-md-4"]}
+                      properties={[
+                        {
+                          label: "Company (disabled)",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Company",
+                          defaultValue: "ColdBlocks",
+                          disabled: true
+                  
+                        },
+                        {
+                          label: "Distributor ID",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Distributor ID",
+                          onChange:this.idChange,
+                          name: "dId"
+                        },
+                        {
+                          label: "Name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Distributor Name",
+                          onChange:this.nameChange,
+                          name: "dName"
+                        }
+                      ]}
+                    />       
+                    <Button bsStyle="success" pullRight fill type="submit">
+                      Submit
+                    </Button>
+                    <div className="clearfix" />
+                  </form>
+                }
+              />
+            </Col>
+          </Row>
+        </Grid>
         <Grid fluid>
           <Row>
             <Col md={12}>
@@ -46,7 +135,7 @@ class ManufacturerList extends Component {
 
                     </thead>
                     <tbody>                     
-                      {Array.isArray(character) && character.map(object => (
+                      {Array.isArray(apiData) && apiData.map(object => (
                         <>
                           <tr>
                             <td>{object.manufacturerID}</td>
