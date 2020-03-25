a1c033330de4243a3b3199f31c397f7b5e080954
mojojojo20 <menonk484@gmail.com>
2020-03-25 15:05:05 +0530

    :boom: add modal to consumer list

--- coldblocks-react/src/views/consumerlist.jsx
+++ coldblocks-react/src/views/consumerlist.jsx
@@ -6,6 +6,8 @@ import { FormInputs } from "components/FormInputs/FormInputs.jsx";
 import Button from "components/CustomButton/CustomButton.jsx";
 import axios from 'axios';
 import { nodeURL } from "variables/Variables.jsx";
+import * as ReactBootstrap from 'react-bootstrap';
+import 'remixicon/fonts/remixicon.css'
 class ConsumerList extends Component {
   constructor() {
     super()
@@ -15,6 +17,11 @@ class ConsumerList extends Component {
       cId: '',
       postD: 0
     }
+     // this.handleShow = this.handleShow.bind(this);
+		this.handleClose = this.handleClose.bind(this);
+		this.state = {
+			show: false,
+		};
   }
   nameChange = event => {
     console.log("Ivnoked nameChange Event handleChange: "+event.target.value);
@@ -38,6 +45,10 @@ class ConsumerList extends Component {
 
     // console.log("user "+user.cId);
     // console.log("user "+user.cName);
+    
+    this.setState({ show: true }, ()=>{
+      console.log("Set State for Show")
+    });  
     
     await axios.post(nodeURL+`/api/CreateConsumer`, 
     { headers: {
@@ -80,13 +91,41 @@ class ConsumerList extends Component {
           apiData : fetchData
         })
       })
+    }
   }
-}
 
+  handleClose() {
+    this.setState({ show: false });
+    window.location.reload();
+	}
+
+	// handleShow() {
+	// 	this.setState({ show: true });
+	// }
   render() {
     const {apiData} = this.state;
+    var Modal = ReactBootstrap.Modal;
     return (
       <div className="content">
+        <Modal show={this.state.show} onHide={this.handleClose}
+              {...this.props}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Transaction Success</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="text-center">
+            <i className="ri-emotion-laugh-line ri-10x text-success"></i>
+            <p className="text-success">Transaction Was Completed Successfully</p>
+          </Modal.Body>
+          <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose}>
+                Close
+              </Button>
+          </Modal.Footer>
+        </Modal>
           <Grid fluid>
           <Row>
             <Col md={12}>
