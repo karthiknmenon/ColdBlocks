5112f220d9273ed7b6b90705abfb34b801720f34
mojojojo20 <menonk484@gmail.com>
2020-03-27 23:52:33 +0530

    :zap: add matrix.py

--- coldblocks-route/matrix.py
+++ coldblocks-route/matrix.py
@@ -8,23 +8,11 @@ import urllib
 def create_data():
   """Creates the data."""
   data = {}
-#   data['API_key'] = 'AIzaSyCTkzOzDdOn1vBbnVvLcf9WYuiypjtFO08'
+  # data['API_key'] = 'AIzaSyCTkzOzDdOn1vBbnVvLcf9WYuiypjtFO08'
   data['addresses'] = ['3610+Hacks+Cross+Rd+Memphis+TN', # depot
                        '1921+Elvis+Presley+Blvd+Memphis+TN',
                        '149+Union+Avenue+Memphis+TN',
                        '1034+Audubon+Drive+Memphis+TN',
-                       '1532+Madison+Ave+Memphis+TN',
-                       '706+Union+Ave+Memphis+TN',
-                       '3641+Central+Ave+Memphis+TN',
-                       '926+E+McLemore+Ave+Memphis+TN',
-                       '4339+Park+Ave+Memphis+TN',
-                       '600+Goodwyn+St+Memphis+TN',
-                       '2000+North+Pkwy+Memphis+TN',
-                       '262+Danny+Thomas+Pl+Memphis+TN',
-                       '125+N+Front+St+Memphis+TN',
-                       '5959+Park+Ave+Memphis+TN',
-                       '814+Scott+St+Memphis+TN',
-                       '1005+Tillman+St+Memphis+TN'
                       ]
   return data
 
