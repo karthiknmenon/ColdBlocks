aed125c8ddbea00aefe4dfd21c93d3ba00d94e1f
mojojojo20 <menonk484@gmail.com>
2020-04-07 01:58:32 +0530

    :zap: add fetcById feature for consumers

--- coldblocks-react/src/views/Normalconsumerlist.jsx
+++ coldblocks-react/src/views/Normalconsumerlist.jsx
@@ -1,7 +1,9 @@
 
 import React, { Component } from "react";
-import { Grid, Row, Col, Table } from "react-bootstrap";
+import { Grid, Row, Col, Table, Modal } from "react-bootstrap";
 import { Card } from "components/Card/Card.jsx";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
 import axios from 'axios';
 import { nodeURL } from "variables/Variables.jsx";
 class NormalConsumerList extends Component {
@@ -11,7 +13,12 @@ class NormalConsumerList extends Component {
       apiData:{},
       cName: '',
       cId: '',
+      fetchId: '',
       postD: 0
+    }
+    this.fetchHandleClose = this.fetchHandleClose.bind(this);
+    this.state = {
+      fetchShow: false,
     }
   }
   componentDidMount() {
@@ -24,29 +31,102 @@ class NormalConsumerList extends Component {
     })
     .catch(console.log)
   }
-  componentDidUpdate(prevProps, prevState) {
-
-    if (prevState.postD !== this.state.postD) {
-      console.log("before new fetch"+JSON.stringify(this.state.apiData))
-      console.log('postD state has changed. (inside didUpdate now)'+this.state.postD);
-      axios.get(nodeURL+`/api/ListConsumers`)
-      .then(res => {
-        console.log(JSON.stringify(res.data))
-        const fetchData = JSON.stringify(res.data);
-        this.setState({
-          apiData : fetchData
-        })
-      })
+  fetchHandleChange = event => {
+    console.log("Invoked fetch ID change: "+event.target.value);
+    this.setState({
+            fetchId: event.target.value
+    })
   }
-}
+  // To query wrt ID 
+  fetchHandleSubmit =  async event => {
+    event.preventDefault();
+      const user = {
+        cId: String(this.state.fetchId)
+      };
+      axios.get(nodeURL+`/api/ListConsumerId?cID=`+user.cId)
+      .then(res => {
+          // console.log(res)
+          var data = res.data
+          console.log(data.status);
+          if(data.status=="error"){
+                  this.setState({
+                    fShow: true
+                })  
+          }else{
+              this.setState({
+              fetchShow: true, fetchName : data.consumerName
+            }) 
+          }               
+      })  
+  }
+
+  fetchHandleClose() {
+    this.setState({ fetchShow: false });
+    this.fetchData();
+  }
+  fetchData(){
+    fetch(nodeURL+'/api/ListConsumers')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ loading: false, apiData: data })
+      // console.log(data);
+    })
+    .catch(console.log)
+  }
 
   render() {
     const {apiData} = this.state;
     return (
       <div className="content">
+        <Modal show={this.state.fetchShow} onHide={this.fetchHandleClose}
+              {...this.props}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Customer Found</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="text-center">
+            <i className="ri-user-search-line ri-10x text-success"></i>
+            <p className="text-success"><b>Success</b></p>
+            <p className="text-dark">Customer with Customer-ID <b>{this.state.fetchId}</b> found with Name <b>{this.state.fetchName}</b></p>
+          </Modal.Body>
+          <Modal.Footer>
+              <Button variant="secondary" onClick={this.fetchHandleClose}>
+                Close
+              </Button>
+          </Modal.Footer>
+        </Modal>
           <Grid fluid>
           <Row>
-            <Col md={12}>
+          <Col md={4}>
+              <Card
+                title="Query Consumer"
+                category="Query Consumer wrt Consumer ID"
+                content={
+                  <form onSubmit={this.fetchHandleSubmit} >
+                    <FormInputs 
+                      ncols={["col-md-12"]}
+                      properties={[
+                        {
+                          label: "Consumer ID",
+                          type: "text",
+                          bsClass: "form-control",
+                          onChange: this.fetchHandleChange,
+                          placeholder: "Enter Consumer ID",                             
+                        },
+                      ]}
+                    />       
+                    <Button bsStyle="success" pullRight fill type="submit">
+                      Submit
+                    </Button>
+                    <div className="clearfix" />
+                  </form>
+                }
+              />
+            </Col>
+            <Col md={8}>
               <Card
                 title="Consumer Details"
                 category="Consumer Details with ID and Name"
