8be617c578be11f906028af7634e50dbca0e972a
mojojojo20 <menonk484@gmail.com>
2020-04-07 02:50:09 +0530

    :zap: fetchId for Distributors

--- coldblocks-react/src/views/Normaldistributorlist.jsx
+++ coldblocks-react/src/views/Normaldistributorlist.jsx
@@ -1,9 +1,11 @@
 
 import React, { Component } from "react";
-import { Grid, Row, Col, Table } from "react-bootstrap";
+import { Grid, Row, Col, Table,Modal } from "react-bootstrap";
 import Card from "components/Card/Card.jsx";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
 import { nodeURL } from "variables/Variables.jsx";
-
+import axios from 'axios';
 class NormalDistributorList extends Component {
   constructor() {
     super()
@@ -11,8 +13,12 @@ class NormalDistributorList extends Component {
       apiData:{},
       dName: '',
       dId: '',
-      postD: 0
+      postD: 0,
+      fetchId: '',
+      fetchName: '',
+      fetchShow: false
     }
+    this.fetchHandleClose = this.fetchHandleClose.bind(this);
   }
 
   componentDidMount() {
@@ -24,6 +30,11 @@ class NormalDistributorList extends Component {
       this.setState({ apiData: data })
     })
     .catch(console.log)
+  }
+  fetchHandleChange = event => {
+    this.setState({
+        fetchId : event.target.value
+    })
   }
   componentDidUpdate(prevProps, prevState) {
     if (prevState.postD !== this.state.postD) {
@@ -39,14 +50,99 @@ class NormalDistributorList extends Component {
       })
       .catch(console.log)
   }
-}
+  }
+  // To query wrt ID 
+  fetchHandleSubmit =  async event => {
+    event.preventDefault();
+    const user = {
+      dID: String(this.state.fetchId)
+    };
+    axios.get(nodeURL+'/api/ListDistributorsId?dID='+user.dID)
+    .then(res => {
+        // console.log(res)
+        var data = res.data
+        console.log(data.status);
+        if(data.status=="error"){
+                this.setState({
+                  fShow: true
+              })  
+        }else{
+            this.setState({
+            fetchShow: true, fetchName : data.distributorName
+          }) 
+        }
+            
+    })
+
+  }
+
+  fetchData(){
+    fetch(nodeURL+'/api/ListDistributors')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({loading: false, apiData: data })
+      // console.log(data);
+    })
+    .catch(console.log)
+  }
+
+  fetchHandleClose() {
+    this.setState({ fetchShow: false });
+    this.fetchData();
+	}
   render() {
     const {apiData} = this.state;
     return (
       <div className="content">
+        <Modal show={this.state.fetchShow} onHide={this.fetchHandleClose}
+              {...this.props}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Distributor Found</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="text-center">
+            <i className="ri-user-search-line ri-10x text-success"></i>
+            <p className="text-success"><b>Success</b></p>
+            <p className="text-dark">Distributor with Distributor-ID <b>{this.state.fetchId}</b> found with Name <b>{this.state.fetchName}</b></p>
+          </Modal.Body>
+          <Modal.Footer>
+              <Button variant="secondary" onClick={this.fetchHandleClose}>
+                Close
+              </Button>
+          </Modal.Footer>
+        </Modal>
         <Grid fluid>
           <Row>
-            <Col md={12}>
+          <Col md={4}>
+              <Card
+                title="Query Distributor"
+                category="Query Distributor wrt Distributor ID"
+                content={
+                  <form onSubmit={this.fetchHandleSubmit} >
+                    <FormInputs 
+                      ncols={["col-md-12"]}
+                      properties={[
+                        {
+                          label: "Distributor ID",
+                          type: "text",
+                          bsClass: "form-control",
+                          onChange: this.fetchHandleChange,
+                          placeholder: "Enter Distributor ID",                             
+                        },
+                      ]}
+                    />       
+                    <Button bsStyle="success" pullRight fill type="submit">
+                      Submit
+                    </Button>
+                    <div className="clearfix" />
+                  </form>
+                }
+              />
+            </Col>
+            <Col md={8}>
               <Card
                 title="Distributor Details"
                 category="Distributor Details with ID and Name"
