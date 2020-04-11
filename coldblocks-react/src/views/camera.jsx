d621fbf5f6b2132423f3a160168f86a35e817ead
mojojojo20 <menonk484@gmail.com>
2020-04-12 03:33:28 +0530

    test qr-code modal feature

--- coldblocks-react/src/views/camera.jsx
+++ coldblocks-react/src/views/camera.jsx
@@ -3,34 +3,62 @@ import {
   Grid,
   Row,
   Col,
-  Button
+  Button,
+  Modal
 } from "react-bootstrap";
 import { Card } from "components/Card/Card.jsx";
 import QrReader from 'react-qr-reader';
 import axios from "axios";
 import {reactURl, nodeURL} from "../variables/Variables"
 class ColdAR extends React.Component {
-  state = {
-    result: 'No result'
+  constructor(){
+    super();
+    this.state = {
+      result: 'No result',
+      show: false,
+    }
+    this.handleClose = this.handleClose.bind(this);
   }
+  
   handleScan = data => {
-    if (data) {
-      this.setState({
-        result: data
-      })
+    if (data) {      
       axios.get(String(this.state.result))
       .then(res => {
         console.log(JSON.stringify(res.data))
       })
-      
+      this.setState({
+        result: data, 
+      })
     }
   }
   handleError = err => {
     console.error(err)
   }
+  handleClose() {
+    this.setState({ show: false });    
+	}
   render() {
     return (
       <div className="content">
+        <Modal show={this.state.show} onHide={this.handleClose}
+              {...this.props}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Transaction Success</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="text-center">
+            <i className="ri-emotion-laugh-line ri-10x text-success"></i>
+            <p className="text-success">Holder Change was Completed Successfully</p>
+          </Modal.Body>
+          <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose}>
+                Close
+              </Button>
+          </Modal.Footer>
+        </Modal>
       <Grid fluid>
         <Col md={12}>
           <Row>
