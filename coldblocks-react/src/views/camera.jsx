85cb6178525f422853cc3d33a5412f9eaa49d971
mojojojo20 <menonk484@gmail.com>
2020-04-13 02:59:33 +0530

    :boom: download dynamic QR-Code for HolderChange from front-end

--- coldblocks-react/src/views/camera.jsx
+++ coldblocks-react/src/views/camera.jsx
@@ -3,11 +3,13 @@ import {
   Grid,
   Row,
   Col,
-  Button,
   Modal
 } from "react-bootstrap";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
 import { Card } from "components/Card/Card.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
 import QrReader from 'react-qr-reader';
+import QRCode from "qrcode.react"
 import axios from "axios";
 import {reactURl, nodeURL} from "../variables/Variables"
 class ColdAR extends React.Component {
@@ -16,6 +18,7 @@ class ColdAR extends React.Component {
     this.state = {
       result: 'No result',
       show: false,
+      pId: '',
     }
     this.handleClose = this.handleClose.bind(this);
   }
@@ -36,7 +39,25 @@ class ColdAR extends React.Component {
   }
   handleClose() {
     this.setState({ show: false });    
-	}
+  }
+  idChange = event => {
+    console.log("Invoked idChange Event handleChange: "+event.target.value);
+    this.setState({
+                    pId: event.target.value });
+  }
+  downloadQR = () => {
+    const canvas = document.getElementById("123456");
+    const pngUrl = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    var fileName = String(localStorage.getItem('username')) + String('-'+Math.floor(Math.random() * 101));
+    downloadLink.download = fileName+".png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
   render() {
     return (
       <div className="content">
@@ -60,25 +81,75 @@ class ColdAR extends React.Component {
           </Modal.Footer>
         </Modal>
       <Grid fluid>
-        <Col md={12}>
+        <Row>
+          <Col md={12}>
+            <Card
+                category="Generate QR-Code for HolderChange Event. Download and Print the QR-Code on the Package. QR-Code changes dynamically on input of Package-ID"
+                content={
+                  <>   
+                    <form onSubmit={this.handleSubmit} name="editInfo" >
+                      <FormInputs 
+                        ncols={["col-md-6", "col-md-6"]}
+                        properties={[
+                          {
+                            label: "Company",
+                            type: "text",
+                            disabled: true,
+                            defaultValue : "ColdBlocks",
+                            bsClass: "form-control",                                                               
+                          },
+                          {
+                            label: "Package ID",
+                            type: "text",
+                            bsClass: "form-control",
+                            placeholder: "Package ID",
+                            onChange:this.idChange,
+                            name: "packageId",                            
+                          },
+                        ]}
+                        />       
+                        <div className="clearfix" />
+                        {/* <Button bsStyle="success" fill type="submit" >
+                          Submit
+                        </Button> */}
+                    </form>
+                    <hr />
+                    <div  className="text-center">
+                      <QRCode
+                        id="123456"
+                        value={"https://bb404505.ngrok.io/qrHolderChange?packageID="+this.state.pId}
+                        size={290}
+                        level={"H"}
+                        includeMargin={true}
+                      /> <br />
+                      <Button bsStyle="success" fill onClick={this.downloadQR}>
+                          Download QR
+                      </Button>                                                  
+                    </div>                             
+                </>
+                }
+              />
+          </Col>
+        </Row>
           <Row>
-          <Card
-              category="ColdAR is an Augmented Reality enabled QR Scanner that allows a user to scan a product QR code to verify its status. A status -ok- means that the package is not tampered, else it's tampered."
-              content={
-                <>
-                  {/* // <Button  pullRight bsStyle="primary" href="https://coldblocksar.netlify.com" target="_blank">Launch ColdAR</Button> */}
-                  <QrReader
-                  delay={300}
-                  onError={this.handleError}
-                  onScan={this.handleScan}
-                  style={{ width: '100%'}}
+            <Col md={12}>
+              <Card
+                  category="ColdAR is an Augmented Reality enabled QR Scanner that allows a user to scan a product QR code to verify its status. A status -ok- means that the package is not tampered, else it's tampered."
+                  content={
+                    <>
+                      {/* // <Button  pullRight bsStyle="primary" href="https://coldblocksar.netlify.com" target="_blank">Launch ColdAR</Button> */}
+                      <QrReader
+                      delay={300}
+                      onError={this.handleError}
+                      onScan={this.handleScan}
+                      style={{ width: '100%'}}
+                    />
+                    {/* <p className="text-muted text-center">{this.state.result}</p> */}
+                  </>
+                  }
                 />
-                {/* <p className="text-muted text-center">{this.state.result}</p> */}
-              </>
-              }
-            />
+            </Col>
           </Row>
-        </Col>
       </Grid>
     </div>          
     )
